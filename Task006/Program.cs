600386bea69a128b69de1c2ee0962c4774f8a81d
Anastasia Ageeva <anastasia.o.ageeva@gmail.com>
2022-07-09 20:10:39 +0300

    Add Task 006

--- Task006/Program.cs
+++ Task006/Program.cs
@@ -3,3 +3,15 @@
 //4 -> да
 //-3 -> нет
 //7 -> нет
+
+Console.Clear();
+Console.Write("Введите целое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
